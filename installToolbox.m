8a5395c1035f6172332d8c44d26b987810d5a1b9
CaiPotato <qingyuanliu@bupt.edu.cn>
2024-04-23 21:03:44 +0800

    123

---
+++ installToolbox.m
@@ -0,0 +1,12 @@
+function installToolbox()
+scriptDir = fileparts(mfilename("fullpath"));
+addpath(fullfile(scriptDir,'Matlab','TACOS'));
+addpath(fullfile(scriptDir,'Python','TACOS','resources','coefficient'));
+addpath(fullfile(scriptDir,'Python','TACOS','resources','default_variance'));
+addpath(fullfile(scriptDir,'Python','TACOS','resources','overlap'));
+addpath(fullfile(scriptDir,'Python','TACOS','resources','region_order'));
+addpath(fullfile(scriptDir,'Python','TACOS','resources','threshold'));
+savepath;
+disp('Toolbox installation is complete');
+end
+
